c85a8445db6a5298537fb99613e8bb0c26ab725f
Oliver Knoll <starsky.hutch@gmx.ch>
2019-12-24 13:39:05 +0100

    Another parenthese variant (non-working, as it seems)

--- apps/permutation/bracket/bracket.go
+++ apps/permutation/bracket/bracket.go
@@ -30,6 +30,32 @@ func DistributeBrackets(n int) map[string]bool {
 	}
 }
 
+// DistributeParentheses distributes n opening and closing
+// parentheses ( and ) in such a way that all valid combinations
+// are returned
+// E.g. n = 2 : ()(), (())
+//      n = 3 : ()()(), (()()), (())(), ()(()), ((())) [but e.g. not )()()(]
+// http://blog.gainlo.co/index.php/2016/12/23/uber-interview-questions-permutations-parentheses/
+// Note: this does not seem to work. The idea seems right, but:
+//       * size of array seems to short: tot = 2*n only (with n=2 we get ()()(())) already, which
+//         has len = 8 > 2 * 2 = 4
+//       * the second if left < right-condition does not adjust pos, so if left > 0 and left < right
+//         then arr[pos] is overwritten      
+func DistributeParentheses(left, right, tot int, arr []rune) {
+	if left == 0 && right == 0 {
+		return
+	}
+	pos := tot - left - right
+	if left > 0 {
+		arr[pos] = rune('(')
+		DistributeParentheses(left -1, right, tot, arr)
+	}
+	if left < right {
+		arr[pos] = rune(')')
+		DistributeParentheses(left, right-1, tot, arr)
+	}
+}
+
 func main() {
 	var n int
 	var err error
@@ -39,11 +65,18 @@ func main() {
 		err = errors.New("No arg")
 	}
 	if err == nil {
+		
 		brackets := DistributeBrackets(n)
 		for k := range brackets {
 			fmt.Printf("%v, ", k)
 		}
 		fmt.Println("")
+
+		tot := 2*n
+		arr := make([]rune, tot)
+		DistributeParentheses(n, n, tot, arr)
+		fmt.Println(arr)
+
 	} else {
 		fmt.Println("Parse error: ", err)
 	}	
