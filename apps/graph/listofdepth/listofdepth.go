e0c3f8c52c884f2f3f7c541a6d68c83bb35ebf13
Oliver Knoll <starsky.hutch@gmx.ch>
2020-01-03 17:43:04 +0100

    FIX: no memory corruption when appending to slice

--- apps/graph/listofdepth/listofdepth.go
+++ apps/graph/listofdepth/listofdepth.go
@@ -7,35 +7,35 @@ import (
 	"github.com/till213/back-to-school/data/tree"
 )
 
-func dfs(n *tree.Node, depth int, lod []*list.List) {
+func depthFirst(n *tree.Node, depth int, lod *[]*list.List) {
 	if n == nil {
 		return
 	}
 
 	// Add level of depth, if necessary
-	if len(lod) < depth {
-		lod = append(lod, list.New())
+	if len(*lod) < depth {
+		*lod = append(*lod, list.New())
 	}
 
-	lod[depth - 1].PushBack(n)
+	(*lod)[depth - 1].PushBack(n)
 	if n.Left != nil {
-		dfs(n.Left, depth + 1, lod)
+		depthFirst(n.Left, depth + 1, lod)
 	}
 	if n.Right != nil {
-		dfs(n.Right, depth + 1, lod)
+		depthFirst(n.Right, depth + 1, lod)
 	}
 }
 
 func listOfDepth(root *tree.Node) []*list.List {
 	lod := make([]*list.List, 0, 8)
 	if root != nil {
-		dfs(root, 1, lod)
+		depthFirst(root, 1, &lod)
 	}
 	return lod
 }
 
 func main() {
-	sorted := []int{1, 2, 3}
+	sorted := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16}
 	root := tree.CreateBinarySearchTree(sorted)
 	
 	lod := listOfDepth(root)
@@ -43,8 +43,9 @@ func main() {
 	for i, v := range lod {
 		fmt.Println("Depth", i+1)
 		for e := v.Front(); e != nil; e = e.Next() {
-			fmt.Printf("%v ->", e.Value.(*tree.Node).Value)
+			fmt.Printf("%v -> ", e.Value.(*tree.Node).Value)
 		}
+		fmt.Println()
 	}
 	fmt.Println()
 }
