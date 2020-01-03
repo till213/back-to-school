ce546644d0ac1bba36c76ebf1200092dab7cd8d1
Oliver Knoll <starsky.hutch@gmx.ch>
2020-01-03 17:51:59 +0100

    Valid BST

---
+++ apps/graph/validbst/validbst.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/till213/back-to-school/data/tree"
+)
+
+func isValidBST(n *tree.Node) bool {
+	var leftValid, rightValid bool
+	if n == nil || n.Left == nil && n.Right == nil {
+		return true
+	} else if n.Left != nil && n.Left.Value > n.Value || n.Right != nil && n.Right.Value < n.Value {
+		return false
+	}
+
+	leftValid = isValidBST(n.Left)
+	rightValid = isValidBST(n.Right)
+	return leftValid && rightValid
+}
+
+func main() {
+	sorted := []int{1, 2, 3, 4}
+	root := tree.CreateBinarySearchTree(sorted)
+	valid := isValidBST(root)
+	fmt.Println("Tree (sorted) is a valid Binary Search Tree:", valid)
+
+	unsorted := []int{3, 2, 1}
+	root = tree.CreateBinarySearchTree(unsorted)
+	valid = isValidBST(root)
+	fmt.Println("Tree (unsorted) is a valid Binary Search Tree:", valid)
+}
